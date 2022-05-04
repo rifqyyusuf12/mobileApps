b5e33ebe286f3c8bfe375397678352d99a147ccc
rifqyyusuf <rifqy@importir.co>
2022-05-04 13:50:11 +0700

    done

--- app/components/Test.js
+++ app/components/Test.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
-function Test(props) {
-    return (
-        <View style={styles.container}></View>
-    );
+export default function Test() {
+  return (
+    <View>
+      <Text>Test</Text>
+    </View>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {}
-});
-
-export default Test;
+const styles = StyleSheet.create({})
