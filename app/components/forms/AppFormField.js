5a2b3eddb5461a012ba2d4e0b7bcfc939e55e933
rifqyyusuf <rifqy@importir.co>
2022-05-06 16:31:08 +0700

    start

--- app/components/forms/AppFormField.js
+++ app/components/forms/AppFormField.js
@@ -14,6 +14,7 @@ function AppFormField({ name, width, ...otheProps }) {
     } = useFormikContext()
 
     return (
+        
         <>
         <AppTextInput
             onBlur= {() => setFieldTouched(name) }
